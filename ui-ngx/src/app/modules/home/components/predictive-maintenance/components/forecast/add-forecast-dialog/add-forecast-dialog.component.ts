07755681839c3219df771e97429435dd8ed9b4f6
skinnyleg <haitammoubal7@gmail.com>
2024-10-07 14:58:21 +0100

    the predictive maintenance page with all it's different tabs and componants

---
+++ ui-ngx/src/app/modules/home/components/predictive-maintenance/components/forecast/add-forecast-dialog/add-forecast-dialog.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common'; // Needed for common directives
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-forecast-dialog',
+  templateUrl: './add-forecast-dialog.component.html',
+  styleUrls: ['./add-forecast-dialog.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+  ]
+})
+export class AddForecastDialogComponent {
+  devices: string[] = []; // Assume you fetch this from a backend service
+  fields: string[] = [];
+
+  selectedDevice: string | null = null;
+
+  constructor(public dialogRef: MatDialogRef<AddForecastDialogComponent>) {}
+
+  ngOnInit() {
+    // TODO: Fetch devices from backend
+    this.devices = ['Device A', 'Device B', 'Device C'];
+  }
+
+  onDeviceSelected(event: any): void {
+    // TODO: Fetch fields based on selected device
+    console.log('Device selected:', event.value);
+  }
+
+  addField(): void {
+    this.fields.push('');
+  }
+
+  removeField(index: number): void {
+    this.fields.splice(index, 1);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onConfirm(): void {
+    // TODO: Perform save operation
+    this.dialogRef.close({ name: 'Forecast', device: this.selectedDevice, fields: this.fields });
+  }
+}
