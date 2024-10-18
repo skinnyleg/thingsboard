8a37c6ff1de7437b77feca7ad674039ff877a98c
skinnyleg <haitammoubal7@gmail.com>
2024-10-18 12:25:03 +0100

    adding new interfaces

--- ui-ngx/src/app/modules/home/models/predictive-maintenance.models.ts
+++ ui-ngx/src/app/modules/home/models/predictive-maintenance.models.ts
@@ -1,7 +1,26 @@
 export interface Order {
   id: string;
   device: string;
-  user: string;
+  // user: string;
+  date: string;
+  status: string;
+}
+
+export interface ForecastField {
+  key: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface ForecastFieldRequest {
+  key: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface ForecastData {
+  id: string;
+  device: string;
   date: string;
   status: string;
 }
@@ -10,126 +29,126 @@ export const ELEMENT_DATA: Order[] = [
   {
     id: "#20462",
     device: "Hat",
-    user: "Matt Dickerson",
+    // user: "Matt Dickerson",
     date: "2022-05-13",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
   {
     id: "#18933",
     device: "Laptop",
-    user: "Wiktoria",
+    // // user: "Wiktoria",
     date: "2022-05-22",
     status: "Completed",
   },
