acaa54cdc5b29eb02861faebdac23263dbbc77cf
skinnyleg <haitammoubal7@gmail.com>
2024-10-28 11:14:57 +0100

    stored the device id and attributes to pass to the chart

--- ui-ngx/src/app/modules/home/pages/predictive-maintenance/forcast/forcast.module.ts
+++ ui-ngx/src/app/modules/home/pages/predictive-maintenance/forcast/forcast.module.ts
@@ -1,10 +1,8 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AppState } from "@app/core/core.state";
-import {
-  ELEMENT_DATA,
-  Order,
-} from "@app/modules/home/models/predictive-maintenance.models";
+import { ForecastService } from "@app/core/http/forecast.service";
+import { Order } from "@app/modules/home/models/predictive-maintenance.models";
 import { PageComponent } from "@app/shared/public-api";
 import { Store } from "@ngrx/store";
 
@@ -14,27 +12,32 @@ import { Store } from "@ngrx/store";
   styleUrls: ["./forcast.component.scss"],
 })
 export class ForcastComponent extends PageComponent implements Order {
+  deviceId: string; // To pass to the chart
+  Attributes: string[]; // To store the temperature data
+
   constructor(
     protected store: Store<AppState>,
     protected route: ActivatedRoute,
+    private forecastService: ForecastService,
     protected router: Router
   ) {
     super(store);
-    this.init();
   }
 
   changeModel(value: any) {
     this.router.navigateByUrl("/PM/forcast/" + value);
   }
+  ngOnInit(): void {
+    this.init();
+  }
 
   models: Order[];
 
   id: string;
+
   trueId: string;
 
   device: string;
-
-  // user: string;
 
   date: string;
 
@@ -44,14 +47,22 @@ export class ForcastComponent extends PageComponent implements Order {
     this.route.params.subscribe((params) => {
       if (params.id) {
         this.id = params.id;
+        this.fetchForcast(params.id);
       }
     });
-    // this.models = ELEMENT_DATA;
-    // const anomaly = this.models.find((element) => element.id === "#" + this.id);
-    // if (!anomaly) return this.router.navigateByUrl("");
-    // this.device = anomaly.device;
-    // // this.user = anomaly.user;
-    // this.date = anomaly.date;
-    // this.status = anomaly.status;
+  }
+
+  fetchForcast(forcastId: string): any {
+    this.forecastService.getForecast(forcastId).subscribe(
+      (data) => {
+        this.deviceId = data.deviceId.id;
+        this.Attributes = data.attributes.map((attr) => {
+          return attr.key;
+        });
+      },
+      (error) => {
+        console.error("Error fetching forecast:", error);
+      }
+    );
   }
 }
