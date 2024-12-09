dea490fc5a1a2c0656df58ca804cc756ed940690
mylastresort <samytamim42@gmail.com>
2024-12-09 15:54:38 +0100

    update network credentials

--- ui-ngx/extra-webpack.config.js
+++ ui-ngx/extra-webpack.config.js
@@ -67,18 +67,18 @@ module.exports = (config, options) => {
   config.module.rules[2].use[0].options.aot = false;
   const index = config.plugins.findIndex(p => p instanceof ngWebpack.AngularWebpackPlugin);
   let angularWebpackPlugin = config.plugins[index];
-  if (config.mode === 'production') {
-    const angularCompilerOptions = angularWebpackPlugin.pluginOptions;
-    angularCompilerOptions.emitClassMetadata = true;
-    angularCompilerOptions.emitNgModuleScope = true;
-    config.plugins.splice(index, 1);
-    angularWebpackPlugin = new ngWebpack.AngularWebpackPlugin(angularCompilerOptions);
-    config.plugins.push(angularWebpackPlugin);
-    const javascriptOptimizerOptions = config.optimization.minimizer[0].options;
-    delete javascriptOptimizerOptions.define.ngJitMode;
-    config.optimization.minimizer.splice(0, 1);
-    config.optimization.minimizer.unshift(new JavaScriptOptimizerPlugin(javascriptOptimizerOptions));
-  }
+  // if (config.mode === 'production') {
+  //   const angularCompilerOptions = angularWebpackPlugin.pluginOptions;
+  //   angularCompilerOptions.emitClassMetadata = true;
+  //   angularCompilerOptions.emitNgModuleScope = true;
+  //   config.plugins.splice(index, 1);
+  //   angularWebpackPlugin = new ngWebpack.AngularWebpackPlugin(angularCompilerOptions);
+  //   config.plugins.push(angularWebpackPlugin);
+  //   const javascriptOptimizerOptions = config.optimization.minimizer[0].options;
+  //   delete javascriptOptimizerOptions.define.ngJitMode;
+  //   config.optimization.minimizer.splice(0, 1);
+  //   config.optimization.minimizer.unshift(new JavaScriptOptimizerPlugin(javascriptOptimizerOptions));
+  // }
 
   // addTransformerToAngularWebpackPlugin(angularWebpackPlugin, keysTransformer);
 
