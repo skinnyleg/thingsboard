2d662345aef0d557661ef2fa3ba4e1e1e681e116
mylastresort <samytamim42@gmail.com>
2025-09-22 09:46:34 +0100

    fix(ui): dark theme

--- ui-ngx/src/app/modules/home/models/dashboard-component.models.ts
+++ ui-ngx/src/app/modules/home/models/dashboard-component.models.ts
@@ -456,8 +456,8 @@ export class DashboardWidget implements GridsterItem, IDashboardWidget {
   }
 
   updateWidgetParams(detectChanges = true) {
-    this.color = this.widget.config.color || 'rgba(0, 0, 0, 0.87)';
-    this.backgroundColor = this.widget.config.backgroundColor || 'none';
+    this.color = this.widget.config.color || 'var(--tb-dark-mat-text-500)';
+    this.backgroundColor = this.widget.config.backgroundColor || 'var(--tb-dark-widget)';
     this.padding = this.widget.config.padding || '8px';
     this.margin = this.widget.config.margin || '0px';
     this.borderRadius = this.widget.config.borderRadius;
